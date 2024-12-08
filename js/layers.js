05efcc6e46724db407f6f1f03564e59c1365587e
ColdSunset <120289521+Minus10Celcius@users.noreply.github.com>
2024-12-08 15:26:37 -0800

    Update layers.js
    esdtrfgyh

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("The First Difficulty", {
+addLayer("d", {
     name: "Particles", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "First Difficulty", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "d", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -36,4 +36,4 @@ addLayer("The First Difficulty", {
     },
 })
 
-if (hasUpgrade('p', 11)) gain = gain.times(2)
+if (hasUpgrade('d', 11)) gain = gain.times(2)
