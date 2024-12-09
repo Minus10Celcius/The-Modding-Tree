e4aa240468a9894c55245c999c404e6b56e500af
ColdSunset <120289521+Minus10Celcius@users.noreply.github.com>
2024-12-09 10:26:23 -0800

    test

--- js/layers.js
+++ js/layers.js
@@ -2,15 +2,19 @@ addLayer("d", {
     name: "Particles", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "d", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
+    startData() {
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
     color: "#FFFFFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {
+        return player.points
+    }, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -21,24 +25,38 @@ addLayer("d", {
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true},
+    hotkeys: [{
+        key: "p",
+        description: "P: Reset for prestige points",
+        onPress() {
+            if (canReset(this.layer)) doReset(this.layer)
+        }
+    }, ],
+    layerShown() {
+        return true
+    },
 
     upgrades: {
-        11: {
-            title: "Make this whatever you want!",
-            description: "Double your point gain.",
-            cost: new Decimal(1),
-            if hasUpgrade('p', 11) gain = gain.times(2)
+        upgrades: {
+            11: {
+                title: "Make this whatever you want!",
+                description: "Double your point gain.",
+                cost: new Decimal(1),
+                effect() {
+                    if (hasUpgrade('p', 11)) {
+                        return 2;
+                    }
+                    return 1;
+                },
+                effectDisplay() { return "×" + this.effect() }
+            },
+        
+            12: {
+                title: "test",
+                description: "test",
+                cost: new Decimal(2),
+            }
         },
-
-        12: {
-            title: "test",
-            description: "test",
-            cost: new Decimal(2),
-        }
     },
- 
+
 })
